0ca30d48a9b081efaf4720a176b3653d5e6c2479
mo7ub <mohamedgamal657@gmail.com>
2024-03-19 01:48:43 +0200

    test

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -80,12 +80,6 @@ export function Navigation(props: NavigationProps) {
         <div className={classNames("fixed left-0 right-0 flex items-center")}>
           <div className="px-7 py-5 relative z-[60] flex flex-1 items-center justify-between">
             <div className="flex items-center space-x-1.5 ssm:space-x-3 pointer-events-auto">
-              <Link
-                className="block tabbable rounded-full text-xs ssm:text-base"
-                to="/"
-              >
-                <BrandPill clickable />
-              </Link>
             </div>
             <div className="relative pointer-events-auto">
               <LinksDropdown>
