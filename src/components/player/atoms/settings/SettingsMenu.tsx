0d32892af53fc0e0989ad25d20ef0c20f9d00ed4
mo7ub <mohamedgamal657@gmail.com>
2024-03-18 02:41:01 +0200

    Update SettingsMenu.tsx

--- src/components/player/atoms/settings/SettingsMenu.tsx
+++ src/components/player/atoms/settings/SettingsMenu.tsx
@@ -76,7 +76,7 @@ export function SettingsMenu({ id }: { id: string }) {
           className={downloadable ? "opacity-100" : "opacity-50"}
         >
           {t("player.menus.settings.downloadItem")}
-          </Menu.Link>
+        </Menu.Link>
         <Menu.Link
           clickable
           onClick={handleWatchPartyClick}
